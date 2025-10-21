60a32830c5e09aa3279aa22d00e66b50685306bf
jonghyeok lee <83393497+whdgur2451@users.noreply.github.com>
2025-10-21 21:02:31 +1000

    day02
    다트의 변수

---
+++ day02/day02.dart
@@ -0,0 +1,12 @@
+void main() {
+  String myName = '이종혁';
+  int myAge = 27;
+  double myHeight = 178;
+  bool isDebeloper = true;
+  String hobby = '코딩';
+
+  print('안녕하세요! 제 이름은 $myName 입니다.');
+  print('나이는 $myAge살이고 , 키는 $myHeight cm 입니다.');
+  print('개발자입니까? $isDebeloper');
+  print('제 취미는 $hobby입니다.');
+}
---
+++ day02/day02mission.dart
@@ -0,0 +1,10 @@
+void main() {
+  String myName = 'jonghyeok lee';
+  int myAge = 27;
+  int day = 2;
+
+  print('my name is $myName ,');
+  print('my age is $myAge');
+  print('I am learing Dart for $day\s');
+  print('I am learing Dart for ${day}s');
+}
